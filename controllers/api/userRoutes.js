ffcfe079fdc8766059cc540b9ea839fcfa65b868
shemar boyd <shemarlion@gmail.com>
2023-03-04 14:57:01 -0500

    Fixed model

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 
 router.get('/signup', async (req, res) => {
   try {
-    res.render('/signup')
+    res.render('/login')
 ;
   } catch (err) {
     res.status(500).json(err);
